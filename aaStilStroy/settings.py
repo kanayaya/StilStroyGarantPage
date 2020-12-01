f116814ac119cef2a8c39101e269360e823e4515
kanayaya <kanayaya@users.noreply.github.com>
2020-12-01 22:22:48 +0800

    I have begun new website writing. Let's go.

--- aaStilStroy/settings.py
+++ aaStilStroy/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-fehw-efufnwufc,vwhdkcjd.vw-jwei%idjEID$$EDIN-sidjhcsu'
+SECRET_KEY = 'u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
